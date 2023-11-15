12fb5cb886df6d063b86fe0856a762d6d3f4bf28
Rahul2818 <rahul@clumpssoftware.com>
2023-11-15 12:43:09 +0530

    added fields in create employee

--- src/pages/employee/create.js
+++ src/pages/employee/create.js
@@ -4,7 +4,7 @@ import HeaderCard from "../../Components/Common/Component/HeaderCard";
 import FooterCard from "../../Components/Forms/FormControl/Common/FooterCard";
 import { Col, Card, CardBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import { H6 } from '../../AbstractElements';
-import { EmailAddress, Website, BillingInformation, CompanyInformation, MegaForm, AccountInformation, ContactNumber, CompanyName, YourName } from '../../Constant';
+import { EmailAddress, Website, BillingInformation, CompanyInformation, MegaForm, AccountInformation, UploadFile, ContactNumber, CompanyName, YourName } from '../../Constant';
 
 
 export default function CreateEmployee() {
@@ -20,27 +20,57 @@ export default function CreateEmployee() {
                         <Form className="theme-form mega-form">
                             <H6>{AccountInformation}</H6>
                             <FormGroup>
-                                <Label className="col-form-label">{YourName}</Label>
-                                <Input className="form-control" type="text" placeholder="your Name" />
+                                <Label className="col-form-label"> Employee Name</Label>
+                                <Input className="form-control" type="text" placeholder="Employee Name" />
                             </FormGroup>
                             <FormGroup>
-                                <Label className="col-form-label">{EmailAddress}</Label>
+                                <Label className="col-form-label">Email Address</Label>
                                 <Input className="form-control" type="email" placeholder="Enter email" />
                             </FormGroup>
                             <FormGroup>
-                                <Label className="col-form-label">{ContactNumber}</Label>
+                                <Label className="col-form-label">Contact Number</Label>
                                 <Input className="form-control" type="Number" placeholder="Enter contact number" />
                             </FormGroup>
                             <hr className="mt-4 mb-4" />
-                            <H6>{CompanyInformation}</H6>
+                            <H6>Employee Information</H6>
                             <FormGroup>
-                                <Label className="col-form-label">CompanyName</Label>
-                                <Input className="form-control" type="text" placeholder="Company Name" />
+                                <Label className="col-form-label">Employee Address</Label>
+                                <Input className="form-control" type="text" placeholder="Address" />
                             </FormGroup>
                             <FormGroup>
-                                <Label className="col-form-label">{Website}</Label>
-                                <Input className="form-control" type="text" placeholder="Website" />
+                                <Label className="col-form-label">Employee Designation</Label>
+                                <Input className="form-control" type="text" placeholder="Designation" />
                             </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Aadhar Number</Label>
+                                <Input className="form-control" type="text" placeholder="Aadhar Number" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Pan Card</Label>
+                                <Input className="form-control" type="text" placeholder="Enter Your Pan Card" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Bank A/C Number</Label>
+                                <Input className="form-control" type="text" placeholder="Enter Your Account Number" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Bank Name</Label>
+                                <Input className="form-control" type="text" placeholder="Bank Name" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Bank IFSC </Label>
+                                <Input className="form-control" type="text" placeholder="Enter your Ifsc" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label className="col-form-label">Date Of Joining</Label>
+                                <Input className="form-control" type="text" placeholder="Date Of Joining" />
+                            </FormGroup>
+                            <FormGroup className="row">
+                                    <Label className="col-sm-3 col-form-label">{UploadFile}</Label>
+                                    <Col sm="9">
+                                        <Input className="form-control" type="file" />
+                                    </Col>
+                                </FormGroup>
                         </Form>
                         <hr className="mt-4 mb-4" />
                         <H6 attrH6={{ className: 'pb-2' }}>{BillingInformation}</H6>
