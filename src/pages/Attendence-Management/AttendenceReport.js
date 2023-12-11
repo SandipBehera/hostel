95a93af1f2e68e73a35fa9c802efb7755985896b
SandipBehera <sandip.sudip36@gmail.com>
2023-12-11 14:13:30 +0530

    fix the hostel fetching issue

--- src/pages/Attendence-Management/AttendenceReport.js
+++ src/pages/Attendence-Management/AttendenceReport.js
@@ -43,7 +43,7 @@ const AttendenceReport = ({ attendanceData }) => {
   const [msg, setMsg] = useState("");
   const [ID, setID] = useState("");
   const [status, setStatus] = useState("");
-  const [pillTab, setPillTab] = useState('1');
+  const [pillTab, setPillTab] = useState("1");
   const [hostelId, setHostelId] = useState("");
 
   const branchId = localStorage.getItem("branchId");
@@ -69,12 +69,13 @@ const AttendenceReport = ({ attendanceData }) => {
         method: "GET",
       });
       const resproom = await response.json();
-      setHostelData(resproom.data)
-      console.log(resproom.data)
+      setHostelData(
+        resproom.data.filter((key) => key.branch_id === parseInt(branchId))
+      );
+      console.log(resproom.data);
     };
     roomHostel();
   }, []);
-
 
   const hostel_name = hostelData?.map((key) => {
     return { value: key.id, label: key.hostel_name };
@@ -87,9 +88,10 @@ const AttendenceReport = ({ attendanceData }) => {
       body: JSON.stringify({ hostel_id: hostelId }),
     });
     const resproom = await response.json();
-    setData(resproom.data
-      .filter(key=>key.banch_id === parseInt(branchId)));
-    console.log(resproom.data)
+    setData(
+      resproom.data.filter((key) => key.branch_id === parseInt(branchId))
+    );
+    console.log(resproom.data);
   };
 
   const handleActionSelect = async (action, comment) => {
@@ -113,7 +115,6 @@ const AttendenceReport = ({ attendanceData }) => {
         setDropdownOpen(!dropdownOpen);
         handleHostelSelect(hostelId);
         toast.success("Attendance Updated Successfully");
-
       } else {
         toast.error("Something went wrong");
         setModalOpen(!modalOpen);
@@ -125,53 +126,59 @@ const AttendenceReport = ({ attendanceData }) => {
   };
 
   return (
-   
     <Fragment>
-    <Breadcrumbs
-      parent="Student"
-      mainTitle="Attendance"
-      subParent="Attendance Report"
-      title="Attendance Report"
-    />      <Col  className='xl-100 box-col-12'>
+      <Breadcrumbs
+        parent="Student"
+        mainTitle="Attendance"
+        subParent="Attendance Report"
+        title="Attendance Report"
+      />{" "}
+      <Col className="xl-100 box-col-12">
         <Card>
-          
           <CardBody>
-            <Nav className='nav-pills'>
+            <Nav className="nav-pills">
               <NavItem>
-                <NavLink href='#' className={pillTab === '1' ? 'active' : ''} onClick={() => setPillTab('1')}>
+                <NavLink
+                  href="#"
+                  className={pillTab === "1" ? "active" : ""}
+                  onClick={() => setPillTab("1")}
+                >
                   Daily Report
                 </NavLink>
               </NavItem>
-              <NavItem>
-              
-                <NavLink href="#" className={pillTab === '2' ? 'active' : ''} onClick={() => setPillTab('2')}>
-                
+              {/* <NavItem>
+                <NavLink
+                  href="#"
+                  className={pillTab === "2" ? "active" : ""}
+                  onClick={() => setPillTab("2")}
+                >
                   Monthly Report
-                 
                 </NavLink>
-                
-              </NavItem>
+              </NavItem> */}
             </Nav>
             <TabContent activeTab={pillTab}>
-              <TabPane className='fade show' tabId='1'>
-                <P attrPara={{ className: 'mb-0 m-t-30' }}>
+              <TabPane className="fade show" tabId="1">
+                <P attrPara={{ className: "mb-0 m-t-30" }}>
                   <Container>
-                    <Col >
+                    <Col>
                       <Card>
-                      <H5 className="text-center">Daily Attendance Report </H5>
+                        <H5 className="text-center">
+                          Daily Attendance Report{" "}
+                        </H5>
                         <CardHeader>
                           <Row className="align-items-center justify-content-between">
                             <Col xs="auto">
                               <div className="mb-2" style={{ width: "100%" }}>
-                                <Label className="col-form-label">Select Hostel name</Label>
+                                <Label className="col-form-label">
+                                  Select Hostel name
+                                </Label>
                                 <Select
                                   options={hostel_name}
                                   className="js-example-basic-single col-sm-12"
-                                  onChange={(selectedOption) =>
-                                    {handleHostelSelect(selectedOption.value),
-                                    setHostelId(selectedOption.value)}
-                                    
-                                  }
+                                  onChange={(selectedOption) => {
+                                    handleHostelSelect(selectedOption.value),
+                                      setHostelId(selectedOption.value);
+                                  }}
                                 />
                               </div>
                             </Col>
@@ -182,18 +189,18 @@ const AttendenceReport = ({ attendanceData }) => {
                         </CardHeader>
                         <div>
                           <Table>
-                          <thead>
-                            <tr>
-                              <th>S.No.</th>
-                              <th>St. Reg. No.</th>
-                              <th>St. Name</th>
-                              <th>H-Name</th>
-                              <th>Room No</th>
-                              <th>Present/Absent</th>
-                              <th>Action</th>
-                            </tr>
-                          </thead>
-                            
+                            <thead>
+                              <tr>
+                                <th>S.No.</th>
+                                <th>St. Reg. No.</th>
+                                <th>St. Name</th>
+                                <th>H-Name</th>
+                                <th>Room No</th>
+                                <th>Present/Absent</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+
                             <tbody>
                               {data.length > 0 ? (
                                 data.map((item) => (
@@ -203,21 +210,36 @@ const AttendenceReport = ({ attendanceData }) => {
                                     <td>{item.name}</td>
                                     <td>{item.hostel_name}</td>
                                     <td>{item.room_id}</td>
-                                    <td>{item.status === 1 ? "present" : "absent"}</td>
                                     <td>
-                                      {<Input type="select"  className="w-75 bg-primary" onChange={(e) => e.target.value!== "" && toggleDropdown(item.id)} >
-                                      <option value="">Action</option>
+                                      {item.status === 1 ? "present" : "absent"}
+                                    </td>
+                                    <td>
                                       {
-                                        item.status==1 ? <option value={0} >Absent</option> :
-                                        <option value={1} >Present</option>
+                                        <Input
+                                          type="select"
+                                          className="w-75 bg-primary"
+                                          onChange={(e) =>
+                                            e.target.value !== "" &&
+                                            toggleDropdown(item.id)
+                                          }
+                                        >
+                                          <option value="">Action</option>
+                                          {item.status == 1 ? (
+                                            <option value={0}>Absent</option>
+                                          ) : (
+                                            <option value={1}>Present</option>
+                                          )}
+                                        </Input>
                                       }
-                                      </Input>}
                                     </td>
                                   </tr>
                                 ))
                               ) : (
                                 <tr>
-                                  <td colSpan="8" style={{ textAlign: "center" }}>
+                                  <td
+                                    colSpan="8"
+                                    style={{ textAlign: "center" }}
+                                  >
                                     <p style={{ color: "red" }}>
                                       Please Select the hostel Name
                                     </p>
@@ -232,67 +254,67 @@ const AttendenceReport = ({ attendanceData }) => {
                   </Container>
                 </P>
               </TabPane>
-              <TabPane tabId='2'>
-                <P attrPara={{ className: 'mb-0 m-t-30' }}>
+              <TabPane tabId="2">
+                <P attrPara={{ className: "mb-0 m-t-30" }}>
                   <MonthlyAttendanceReport />
                 </P>
               </TabPane>
             </TabContent>
             {/* Modal outside the loop */}
             <Modal isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)}>
-      <ModalHeader toggle={() => setModalOpen(!modalOpen)}>
-        Where are you....
-      </ModalHeader>
-      <ModalBody>
-        <FormGroup>
-          <Label for="exampleText">Write here</Label>
-          <Input
-            type="textarea"
-            name="text"
-            id="exampleText"
-            rows="5"
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-          />
-        </FormGroup>
+              <ModalHeader toggle={() => setModalOpen(!modalOpen)}>
+                Where are you....
+              </ModalHeader>
+              <ModalBody>
+                <FormGroup>
+                  <Label for="exampleText">Write here</Label>
+                  <Input
+                    type="textarea"
+                    name="text"
+                    id="exampleText"
+                    rows="5"
+                    value={msg}
+                    onChange={(e) => setMsg(e.target.value)}
+                  />
+                </FormGroup>
 
-        <FormGroup tag="fieldset">
-          <legend>Status</legend>
-          <FormGroup check>
-            <Label check>
-              <Input
-                type="radio"
-                name="status"
-                value="1"
-                checked={status === 1}
-                onChange={() => setStatus(1)}
-              />{' '}
-              Mark Present
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input
-                type="radio"
-                name="status"
-                value="0"
-                checked={status === 0}
-                onChange={() => setStatus(0)}
-              />{' '}
-              Mark Absent
-            </Label>
-          </FormGroup>
-        </FormGroup>
-      </ModalBody>
-      <ModalFooter>
-        <Button
-          color="secondary"
-          onClick={() => handleActionSelect(status, msg)}
-        >
-          Send
-        </Button>
-      </ModalFooter>
-    </Modal>
+                <FormGroup tag="fieldset">
+                  <legend>Status</legend>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="status"
+                        value="1"
+                        checked={status === 1}
+                        onChange={() => setStatus(1)}
+                      />{" "}
+                      Mark Present
+                    </Label>
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="status"
+                        value="0"
+                        checked={status === 0}
+                        onChange={() => setStatus(0)}
+                      />{" "}
+                      Mark Absent
+                    </Label>
+                  </FormGroup>
+                </FormGroup>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="secondary"
+                  onClick={() => handleActionSelect(status, msg)}
+                >
+                  Send
+                </Button>
+              </ModalFooter>
+            </Modal>
           </CardBody>
         </Card>
       </Col>
@@ -302,32 +324,28 @@ const AttendenceReport = ({ attendanceData }) => {
 
 export default AttendenceReport;
 
-
-
-
-
 // <Dropdown
-                                      //   isOpen={dropdownOpen && ID === item.id}
-                                      //   toggle={() => toggleDropdown(item.id)}
-                                      // >
-                                      //   <DropdownToggle caret>{Action}</DropdownToggle>
-                                      //   <DropdownMenu>
-                                      //     {item.status === 1 ? (
-                                      //       <DropdownItem
-                                      //         onClick={() => {
-                                      //           setStatus(item.status);
-                                      //         }}
-                                      //       >
-                                      //         Make Absent
-                                      //       </DropdownItem>
-                                      //     ) : (
-                                      //       <DropdownItem
-                                      //         onClick={() => {
-                                      //           setStatus(item.status);
-                                      //         }}
-                                      //       >
-                                      //         Make Present
-                                      //       </DropdownItem>
-                                      //     )}
-                                      //   </DropdownMenu>
-                                      // </Dropdown>
+//   isOpen={dropdownOpen && ID === item.id}
+//   toggle={() => toggleDropdown(item.id)}
+// >
+//   <DropdownToggle caret>{Action}</DropdownToggle>
+//   <DropdownMenu>
+//     {item.status === 1 ? (
+//       <DropdownItem
+//         onClick={() => {
+//           setStatus(item.status);
+//         }}
+//       >
+//         Make Absent
+//       </DropdownItem>
+//     ) : (
+//       <DropdownItem
+//         onClick={() => {
+//           setStatus(item.status);
+//         }}
+//       >
+//         Make Present
+//       </DropdownItem>
+//     )}
+//   </DropdownMenu>
+// </Dropdown>
