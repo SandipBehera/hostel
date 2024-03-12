20923877e34856cfede9de6341a73b10d708eb3f
SandipBehera <sandip.sudip36@gmail.com>
2024-03-12 13:10:14 +0530

    added client ip

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 export const LocalApi = `http://localhost:3001/api`;
-export const WebApi = `http://13.58.217.203:3001/api`;
-export const WebSocketAPI = `http://13.58.217.203:3001`;
+export const WebApi = `http://13.58.144.48:3001/api`;
+export const WebSocketAPI = `http://13.58.144.48:3001`;
 export const LocalSocketAPI = `http://localhost:3001`;
 export const LocalStore = `http://localhost:3001/upload/`;
-export const WebStore = `http://13.58.217.203:3001/upload/`;
+export const WebStore = `http://13.58.144.48:3001/upload/`;
